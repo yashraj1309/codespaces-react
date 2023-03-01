41afd7c28e79332c0dbf13da4b0f4a9607ee2356
Yashraj Dudhatra <77092074+yashraj1309@users.noreply.github.com>
2023-03-01 19:55:16 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import Login from './Forrms/Login';
 function App() {
   return (
     <div className="App">
+    <Login />
     </div>
   );
 }
--- src/Forrms/Login.js
+++ src/Forrms/Login.js
@@ -1,6 +1,34 @@
+import { useState,useEffect } from "react";
+import data from "./mobileCode.json";
+
 export default function Login() {
+    const [mobileCode, setMobileCode] = useState(data);
+    const [selectedValue, setSelectedValue] = useState('IN');
+    const handleSelectChange = (event) => {
+        console.log(event.target.value);
+        setSelectedValue(event.target.value);
+    };
     return (
-        <div className="App">
-        </div>
+        <form className="form">
+            <h1>Create Account</h1>
+            <div className="form-field">
+            <p>Your name</p>
+            <input type="name"/>
+            </div>
+            <div className="form-field">
+            <p>Mobile number</p>
+            <div className="mobile">
+            <div className="true">{selectedValue}</div>
+            <select onChange={handleSelectChange}>
+            {mobileCode.map(item => (
+                <option value={item.code}>
+                    <p>{item.name} {item.dial_code}</p>
+                </option>
+            ))}
+      </select>
+            <input type="number"/>
+            </div>
+            </div>
+        </form>
     );
 }
